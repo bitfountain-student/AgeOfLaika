d5d47eb4a78dbac3efa321c3d0ae4314a4df6508
bitfountain-student <bitfountain@slowchat.us>
2015-06-28 10:14:53 -0700

    completed: Age of Laika

--- AgeOfLaika/ViewController.swift
+++ AgeOfLaika/ViewController.swift
@@ -10,6 +10,9 @@ import UIKit
 
 class ViewController: UIViewController {
 
+  @IBOutlet weak var textField: UITextField!
+  
+  @IBOutlet weak var resultLabel: UILabel!
   override func viewDidLoad() {
     super.viewDidLoad()
     // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +23,11 @@ class ViewController: UIViewController {
     // Dispose of any resources that can be recreated.
   }
 
+  @IBAction func buttonPressed(sender: UIButton) {
+    let factor = 7
+    resultLabel.hidden = false
+    resultLabel.text = "\(factor * textField.text.toInt()!) in dog years"
+  }
 
 }
 
